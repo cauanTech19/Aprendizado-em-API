9f2d2881a5db38ddced96cb1626023b7f57f00cf
Cauan Justino <cauanpazjustino@gmail.com>
2026-04-02 17:48:00 -0300

    Modificando app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
 import os
@@ -9,42 +10,31 @@ from models import db, bcrypt
 from auth_user import auth_pb
 from livros import livros_pb
 
-
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
-
 
 db.init_app(app)
 bcrypt.init_app(app)
 JWTManager(app)
 
-
 app.register_blueprint(auth_pb)
 app.register_blueprint(livros_pb)
 
-
 @app.errorhandler(500)
 def erro_interno(e):
-    return jsonify({'Mensage': 'erro interno no servidor'}), 500
-    
+    return jsonify({'Mensagem': 'Erro interno no servidor'}), 500
+
 @app.errorhandler(404)
 def rota_nao_encontrada(e):
     return jsonify({'Mensagem': 'Rota não encontrada'}), 404
 
 @app.errorhandler(405)
-def metodo_nao_permimtido(e):
+def metodo_nao_permitido(e):
     return jsonify({'Mensagem': 'Método não permitido'}), 405
-
-
 
 with app.app_context():
     db.create_all()
 
-__all__ = ['bcrypt']
-
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=False) 
